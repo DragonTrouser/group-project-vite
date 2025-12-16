3d2d2c17372e7411da117a6345db64326e32aa56
DragonTrouser <dtdamianwyrebiak@gmail.com>
2025-12-16 04:04:27 +0100

    Modify RandomButton

--- src/components/RandomButton/RandomButton.jsx
+++ src/components/RandomButton/RandomButton.jsx
@@ -2,15 +2,15 @@ import './RandomButton.css'
 
 export default function RandomButton({
     onCreate,
-    numColors = 5,
     numRods = 6,
     numBoltsPerRod = 6
 }) {
     const handleClick = () => {
+        const numColors = numRods - 1
         const newRods = Array.from({ length: numRods }, () => [])
-        const emptyRodIndex = 0
-        const totalBolts = (numRods - 1) * numBoltsPerRod
 
+        const emptyRodIndex = numRods - 1
+        const totalBolts = (numRods - 1) * numBoltsPerRod
         const boltsPerColor = Math.floor(totalBolts / numColors)
 
         const allBolts = []
@@ -21,7 +21,8 @@ export default function RandomButton({
         }
 
         for (let i = allBolts.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1)); [allBolts[i], allBolts[j]] = [allBolts[j], allBolts[i]]
+            const j = Math.floor(Math.random() * (i + 1))
+            ;[allBolts[i], allBolts[j]] = [allBolts[j], allBolts[i]]
         }
 
         let rodIndex = 0
